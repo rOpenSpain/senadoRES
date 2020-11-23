3332436b95160db106a55396a72be5d2cb341f87
Lluís <lluis.revilla@gmail.com>
2020-11-23 23:59:01 +0100

    Autors if present now are lists

--- R/diario.R
+++ R/diario.R
@@ -1,5 +1,4 @@
-
-#' Retrieve the document of sumario
+#' Retrieve the document of _sumario_
 #'
 #' Returns the summary of a session in a tidy way.
 #' If multiple autors are involved in a disposicion and multiple documents they
@@ -9,20 +8,20 @@
 #' @family types of documents
 #' @export
 #' @examples
-#' sumario_cve <- sumario_nbo(14, 1)
-#' head(sumario(sumario_cve))
+#' sumario_CVE <- sumario_cve(14, 3)
+#' head(sumario(sumario_CVE))
 sumario <- function(cve) {
     check_code(cve)
     cve <- fix_sumario_code(cve) # Fix till they fix on the website
-    # XML of sumarios are on the pdf folder.
+    # For some reason the xml files of the sumarios are on the pdf folder too...
     url <- paste0("/legis", id2legis(cve), "/publicaciones/pdf/senado/bocg/",
                   cve, ".XML")
     base_url <- force(BASE_URL)
     url <- httr::modify_url(base_url, path = url)
     xml <- get_xml(url)
 
-    header <- xml2matrix(xml_find_all(xml, "/boletin/cabecera"))
-    apartados <- xml_find_all(xml, "/boletin/cuerpo/apartados/apartado")
+    header <- xml2matrix(xml_find_all(xml, ".//cabecera"))
+    apartados <- xml_find_all(xml, ".//cuerpo/apartados/apartado")
     # Based on the disposition we merge the apartados
     if (length(xml_find_all(xml, "//disposicion")) > 1) {
         apartados <- lapply(apartados, tidy_apartado)
@@ -38,6 +37,30 @@ sumario <- function(cve) {
     out
 }
 
+tidy_apartado <- function(x) {
+    subapartados <- xml_children(xml_find_all(x, ".//subapartado"))
+    apartado <- xml2matrix2(subapartados[[1]])
+    disposiciones <- xml_children(subapartados[[2]])
+
+    a <- lapply(disposiciones, tidy_disposicion)
+    b <- Reduce(merger, a)
+    head <- xml2matrix2(xml_find_all(x, ".//apNumRomano|apDescripcion"))
+    cbind.data.frame(add_rows(head, a), add_rows(apartado, a), b)
+}
+
+tidy_disposicion <- function(x) {
+    info <- xml2matrix2(
+        xml_find_all(x, "./objeto|tipoExpediente|disp|cve|fase|extra"))
+    info <- as.data.frame(info, row.names = NULL, make.names = FALSE)
+    autor <- xml_text(xml_find_all(x, ".//autor"))
+    if (length(autor) != 0) {
+        # Need to find a document with authors
+        # Set a warning to remind myself
+        info$autor <- list(autor)
+    }
+    info
+}
+
 #' Boletin
 #'
 #' @param cve A character with the boletin CVE.
@@ -45,23 +68,41 @@ sumario <- function(cve) {
 #' @return A data.frame
 #' @seealso [boletin_cve()]
 #' @examples
-#' boletin_CVE <- boletin_cve(14, 2)
-#' boletin(boletin_CVE)
+#' boletin_CVE <- boletin_cve(14, 3)
+#' b <-  boletin(boletin_CVE)
 boletin <- function(cve) {
     check_code(cve)
-    # For some reason the xml files of the sumarios are on the pdf folder...
-    url <- paste0(force(BASE_URL), "/legis", id2legis(cve), "/publicaciones/xml/senado/bocg/",
-                  cve, ".XML")
+    url <- paste0(force(BASE_URL), "/legis", id2legis(cve),
+                  "/publicaciones/xml/senado/bocg/", cve, ".XML")
     xml <- get_xml(url)
     cabecera <- xml2matrix2(xml_children(xml_find_all(xml, "./cabecera"))[1:6])
     disposiciones <- xml_find_all(xml, "./texto_boletin/disposicion")
     dis <- lapply(disposiciones, tidy_long_disposicion)
     dis <- Reduce(rbind, dis)
-    out <- as.data.frame(cbind(add_rows(cabecera, dis), dis),
-                         make.names = FALSE)
+    out <- cbind.data.frame(add_rows(cabecera, dis), dis)
     numerics <- c("NBOL", "ANNO", "numpag", "numpagfin", "DISP")
     out[, numerics] <- lapply(out[, numerics], as.numeric)
     out$FECHA <- as.Date(out$FECHA, "%Y%m%d")
+    ids <- paste(out$CVE, out$DISP, sep = "_")
+    ids <- change_type_document(ids, "D")
+    out$document <- ids
+    out
+}
+
+#' Info of a session
+#'
+#' Merges both information from the [boletin()] and from the [sumario()]
+#' @inheritParams sumario_cve
+#' @return A data.frame
+#' @seealso [boletin()] and [sumario()]
+#' @export
+#' @examples
+#' bs <- boletin_sumario(14, 3)
+boletin_sumario <- function(legislatura, sesion) {
+
+    s <- sumario(sumario_cve(legislatura, sesion))
+    b <- boletin(boletin_cve(legislatura, sesion))
+    out <- cbind(s, b)
     out
 }
 
@@ -79,27 +120,13 @@ documento <- function(cve) {
                   id2legis(cve), "/publicaciones/xml/senado/bocg/", cve, ".XML")
     xml <- read_xml(url)
     cabecera <- xml2matrix2(xml_children(xml_find_all(xml, "./cabecera"))[1:9])
-    disposicion <- tidy_long_disposicion(xml_children(xml_child(xml, "texto_boletin")))
+    disposicion <- tidy_long_disposicion(xml_children(
+        xml_child(xml, "texto_boletin")))
     out <- as.data.frame(cbind(cabecera, disposicion), make.names = FALSE)
     out$FECHA <- as.Date(out$FECHA, "%Y%m%d")
     numerics <- c("NBOL", "ANNO", "numpag", "numpagfin", "DISP")
     out[, numerics] <- lapply(out[, numerics], as.numeric)
     out
-}
-
-
-tidy_apartado <- function(x) {
-    subapartados <- xml_children(xml_find_all(x, ".//subapartado"))
-    apartado <- xml2matrix2(subapartados[[1]])
-    disposiciones <- xml_children(subapartados[[2]])
-
-    a <- lapply(disposiciones, tidy_disposicion)
-    disposiciones <- Reduce(merger, a)
-    head <- xml2matrix2(xml_find_all(x, ".//apNumRomano|apDescripcion"))
-
-    cbind(add_rows(head, disposiciones),
-          add_rows(apartado, disposiciones),
-          disposiciones)
 }
 
 #' @importFrom xml2 xml_attrs
@@ -110,23 +137,9 @@ tidy_long_disposicion <- function(x) {
     header <- xml2matrix2(xml_children(cabecera)[1:2])
     attrs <- xml_attrs(x)
     attrs <- if (is.list(attrs)) {attrs[[1]]}else {attrs}
-    header <- cbind(header, t(as.matrix(attrs)), # Doesn't work well if it is a nodeset
+    header <- cbind.data.frame(header, t(as.matrix(attrs)),
                     xml2matrix(xml_child(cabecera, "item_publicado")))
 
-    text <- xml_text(xml_child(x, "texto_disposicion"))
-    cbind(header, text = text)
+    header$text <- xml_text(xml_child(x, "texto_disposicion"))
+    header
 }
-
-tidy_disposicion <- function(x) {
-    autor <- t(xml2matrix2(xml_find_all(x, ".//autor")))
-    colnames(autor) <- unique(rownames(autor))
-    ficheros <- t(xml2matrix2(xml_find_all(x, ".//fichero")))
-    colnames(ficheros) <- unique(rownames(ficheros))
-    info <- xml2matrix2(
-        xml_find_all(x, "./objeto|tipoExpediente|disp|cve|fase|extra"))
-    pick <- if (nrow(autor) > nrow(ficheros)) autor else ficheros
-    cbind(add_rows(info, pick),
-          add_rows(ficheros, pick),
-          add_rows(autor, pick))
-}
-
